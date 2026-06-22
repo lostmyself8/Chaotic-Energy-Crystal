743c1c1a31b4260977581e6b0a1510396285a7d3
Jerry <1056552046@qq.com>
2026-06-22 23:28:49 +0800

    修复传输速度错误的问题

--- src/main/java/com/jerry/main/mixin/TileCrystalWirelessIOMixin.java
+++ src/main/java/com/jerry/main/mixin/TileCrystalWirelessIOMixin.java
@@ -1,7 +1,11 @@
 package com.jerry.main.mixin;
 
 import com.brandon3055.brandonscore.api.TechLevel;
+import com.brandon3055.draconicevolution.blocks.energynet.tileentity.TileCrystalBase;
 import com.brandon3055.draconicevolution.blocks.energynet.tileentity.TileCrystalWirelessIO;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,16 +13,31 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = TileCrystalWirelessIO.class, remap = false)
-public abstract class TileCrystalWirelessIOMixin {
+public abstract class TileCrystalWirelessIOMixin extends TileCrystalBase {
 
     @Unique
     private static final int CHAOTIC_TIER = TechLevel.CHAOTIC.index;
 
+    public TileCrystalWirelessIOMixin(BlockEntityType<?> tileEntityTypeIn, BlockPos pos, BlockState state) {
+        super(tileEntityTypeIn, pos, state);
+    }
+
+    @Override
+    public int getTier() {
+        return super.getTier();
+    }
+
     @Inject(method = "getMaxReceivers", at = @At("HEAD"), cancellable = true)
     private void chaosEnergyCrystal$getMaxReceivers(CallbackInfoReturnable<Integer> cir) {
-        //不能直接使用@Shadow标记getTier()
-        if (((TileCrystalWirelessIO) (Object) this).getTier() == CHAOTIC_TIER) {
+        if (getTier() == CHAOTIC_TIER) {
             cir.setReturnValue(128);
+        }
+    }
+
+    @Inject(method = "getMaxWirelessTransfer", at = @At("HEAD"), cancellable = true)
+    private void chaosEnergyCrystal$getMaxWirelessTransfer(CallbackInfoReturnable<Integer> cir) {
+        if (getTier() == CHAOTIC_TIER) {
+            cir.setReturnValue(2048000);
         }
     }
 }
